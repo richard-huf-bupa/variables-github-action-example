b41437ba82285a2c136c3bc5922bec9b0f500236
Richard Huf <richard.huf@bupa.com.au>
2025-03-13 16:30:43 +1100

    chore: add flutter and react native tokens for fun

--- src/build.mjs
+++ src/build.mjs
@@ -4,10 +4,10 @@ const sd = new StyleDictionary({
   log: {
     verbosity: 'verbose'
   },
-  source: ['tokens/**/*.json'],
+  source: ['src/tokens/**/*.json'],
   platforms: {
     css: {
-      prefix: 'rds-new',
+      prefix: 'rds-',
       transformGroup: 'css',
       buildPath: 'build/',
       files: [
@@ -19,8 +19,31 @@ const sd = new StyleDictionary({
           }
         },
       ],
+    },
+    flutter: {
+      transformGroup: "flutter",
+      buildPath: "build/",
+      files: [
+        {
+          destination: "style_dictionary.dart",
+          format: "flutter/class.dart",
+          options: {
+            className: "StyleDictionary"
+          }
+        }
+      ]
+    },
+    rn: {
+      transformGroup: "react-native",
+      buildPath: "./src/style-dictionary-dist/",
+      files: [
+        {
+          destination: "variables.js",
+          format: "javascript/es6"
+        }
+      ]
     }
-  },
+  }
 });
 
 await sd.buildAllPlatforms();
